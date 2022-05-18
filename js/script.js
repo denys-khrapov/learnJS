b91088afa20d88e8f47f1e51d222d3951713fa5d
Khrapov Denys <khrapov.denys@gmail.com>
2022-05-18 23:49:48 +0300

    methods and properties of strings and numbers

--- js/script.js
+++ js/script.js
@@ -287,3 +287,42 @@ logger();
 const calcs = (a, b) => {
 	return a + b;
 };
+
+
+//методы и свойства строк и чисел
+
+
+const str = 'teSt';
+const arr = [1, 2, 10];
+
+
+//методы изменения регистра
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+
+//поиск подстроки
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf('fruit'));
+
+
+const logg = 'hello world';
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+
+const numb = 12.2;
+console.log(Math.round(numb));
+
+
+const test = '12.2px';
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
