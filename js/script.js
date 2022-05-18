9722371e94d6750ad9175b9462caf218497c37ac
Khrapov Denys <khrapov.denys@gmail.com>
2022-05-18 14:46:35 +0300

    lesson cycles

--- js/script.js
+++ js/script.js
@@ -111,3 +111,71 @@ console.log(personalMovieDB);
 // const isClose = false;
 
 // console.log(isChecked || isClose);
+
+
+// Условия 
+
+// if (4 == 9) {
+// 	console.log('ok');
+// } else {
+// 	console.log('not ok');
+// }
+
+
+// const num = 50;
+
+// if (num < 49) {
+// 	console.log('переменная меньше!');
+// } else if (num > 100) {
+// 	console.log('переменная больше!');
+// } else {
+// 	console.log('Все ок!');
+// }
+
+//тернарный оператор
+
+// (num === 50) ? console.log('Все окей!'): console.log('Error');
+
+
+//switch
+// const number = 50;
+// switch (number) {
+// 	case 49:
+// 		console.log('Неверно');
+// 		break;
+// 	case 51:
+// 		console.log('Неверно');
+// 		break;
+// 	case 54:
+// 		console.log('Верно');
+// 		break;
+// 	default:
+// 		console.log('Не в этот раз');
+// 		break;
+// }
+
+
+// циклы
+let num = 50;
+
+do {
+	console.log(num);
+	num++;
+} while (num < 55);
+
+
+while (num <= 55) {
+	console.log(num);
+	num++;
+}
+
+
+
+
+for (let i = 1; i < 10; i++) {
+	if (i === 6) {
+		break;
+		// continue;
+	}
+	console.log(i);
+}
